3fd536092c01b7ac9bbe62da5c3ffb40f96ef49d
noomenkrichen <nkrichen@outlook.com>
2021-03-17 10:53:45 +0100

    added filter and search buttons

--- src/App.js
+++ src/App.js
@@ -18,50 +18,68 @@ function App() {
       "Jumanji",
       "This is an adventure movie in the jungle",
       "img/jumanji.jpg",
-      6.6
+      4.0
     ),
     createMovie(
       "Spider-Man",
       "Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.",
       "img/spider-man.jpg",
-      7.5
+      4.5
     ),
     createMovie(
       "Captain America",
       "Political involvement in the Avengers' affairs causes a rift between Captain America and Iron Man.",
       "img/captain-america.jpg",
-      7.8
+      4.8
     ),
     createMovie(
       "The Avengers",
       "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
       "img/avengers.jpg",
-      8.0
+      5.0
     ),
   ]);
 
   const addMovie = () => {
-    const title = prompt("title");
-    const description = prompt("description");
-    const posterUrl = prompt("posterUrl");
-    const rate = prompt("rate");
+    const title = prompt("Title");
+    const description = prompt("Description");
+    const posterUrl = prompt("Poster Url");
+    const rate = prompt("Rate");
     setMovies([...movies, createMovie(title, description, posterUrl, rate)]);
   };
 
-  const filterMovies = () => {};
+  const filterMovies = () => {
+    const arr = [...movies];
+    const rate = prompt("Rate");
+    setMovies(arr.filter((el) => el.rate >=rate))
+  };
+
+  const filterMoviesTitle = () => {
+    const arr = [...movies];
+    const str = prompt("Title");
+    setMovies(arr.filter((el) => el.title.toLowerCase().includes(str.toLowerCase())))
+  };
+
+  const searchMovies = () => {
+    const arr = [...movies];
+    const str = prompt("Word");
+    setMovies(arr.filter((el) => el.title.toLowerCase().includes(str.toLowerCase()) || el.description.toLowerCase().includes(str.toLowerCase())))
+  };
 
   return (
     <div className="App">
       <div id="movies" className="movie-list">
         <MovieList movies={movies} />
       </div>
-      <div id="btn">
+      <div id="btn1">
         <button onClick={addMovie}>Add movie</button>
+        <button onClick={filterMovies}>Filter movie by rate</button>
+        <button onClick={filterMoviesTitle}>Filter movie by title</button>
+        <button onClick={searchMovies}>Search movies by word</button>
       </div>
+      
     </div>
   );
 }
 
 export default App;
-
-//use Modal to input new movie details
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const MovieCard = (props) => {
   return (
-    <>
+    <div className="movie-card">
       <img src={props.posterUrl} alt="poster" />
       <h1>{props.title}</h1>
-      <div>{props.description}</div>
-      <div>{props.rate}</div>
-    </>
+      <div className="desc"><b>Description:</b> {props.description}</div>
+      <div><b>Rate:</b> {props.rate}</div>
+    </div>
   );
 };
 
